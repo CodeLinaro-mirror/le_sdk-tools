a369ebde99b55a7eca40c5aac6857256d69b472c
Ivan Evlogiev <ievlogie@qti.qualcomm.com>
2026-02-03 05:43:06 -0800

    qimsdk-debian: Update build scripts to follow CMakeFile updates
    - Set CMAKE_INSTALL_INCLUDEDIR, CMAKE_INSTALL_BINDIR,
      CMAKE_INSTALL_LIBDIR, CMAKE_INSTALL_SYSCONFDIR
    
    Change-Id: Ie9091a6f5c29c40e4353cdaa24cee4090dd890ec
    Signed-off-by: Ivan Evlogiev <ievlogie@qti.qualcomm.com>

--- qimsdk-debian/scripts/build.sh
+++ qimsdk-debian/scripts/build.sh
@@ -31,10 +31,10 @@ function qimsdk-cmake-configure() {
         local CMAKE_FLAGS="-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON`
             ` -DSYSROOT_INCDIR=/usr/include`
             ` -DSYSROOT_LIBDIR=/usr/lib`
-            ` -DGST_PLUGINS_QTI_OSS_INSTALL_INCDIR=/usr/include`
-            ` -DGST_PLUGINS_QTI_OSS_INSTALL_BINDIR=/usr/bin`
-            ` -DGST_PLUGINS_QTI_OSS_INSTALL_LIBDIR=/usr/lib/aarch64-linux-gnu`
-            ` -DGST_PLUGINS_QTI_OSS_INSTALL_CONFIG=/etc/configs/`
+            ` -DCMAKE_INSTALL_INCLUDEDIR=/usr/include`
+            ` -DCMAKE_INSTALL_BINDIR=/usr/bin`
+            ` -DCMAKE_INSTALL_LIBDIR=/usr/lib/aarch64-linux-gnu`
+            ` -DCMAKE_INSTALL_SYSCONFDIR=/etc`
             ` -DCMAKE_BUILD_TYPE=Debug`
             ` "${CMAKE_CUSTOM_CONFIG_FLAGS}""
 
