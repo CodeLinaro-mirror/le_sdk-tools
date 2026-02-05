baa93e7b673a1bc6c66e62642cdaaa614aeecaaf
Ivan Evlogiev <ievlogie@qti.qualcomm.com>
2026-02-05 11:24:36 +0200

    qimsdk: Update build scripts to follow CMakeFile updates
    - Update CMAKE_INSTALL_INCLUDEDIR, CMAKE_INSTALL_BINDIR,
      CMAKE_INSTALL_LIBDIR
    - Add CMAKE_INSTALL_PREFIX
    - Remove obsolete config flags
    
    Change-Id: Ia1d711b76fcc3df17164bb9b00fcb26a77f8f225
    Signed-off-by: Ivan Evlogiev <ievlogie@qti.qualcomm.com>

--- qimsdk/scripts/dev_image/build.sh
+++ qimsdk/scripts/dev_image/build.sh
@@ -57,13 +57,12 @@ function qimsdk-cmake-configure() {
         qimsdk-setup-crosscompilation
         local CMAKE_FLAGS="
             -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON
-            -DGST_PLUGINS_QTI_OSS_VERSION=1.24
-            -DGST_VERSION_REQUIRED=1.24
             -DSYSROOT_INCDIR=/usr/include
             -DSYSROOT_LIBDIR=/usr/lib
-            -DCMAKE_INSTALL_INCLUDEDIR=/usr/include
-            -DCMAKE_INSTALL_BINDIR=/usr/bin
-            -DCMAKE_INSTALL_LIBDIR=/usr/lib/aarch64-linux-gnu
+            -DCMAKE_INSTALL_PREFIX=/usr
+            -DCMAKE_INSTALL_INCLUDEDIR=include
+            -DCMAKE_INSTALL_BINDIR=bin
+            -DCMAKE_INSTALL_LIBDIR=lib/aarch64-linux-gnu
             -DCMAKE_INSTALL_SYSCONFDIR=/etc
             -DCMAKE_BUILD_TYPE=Debug
             ${CMAKE_CUSTOM_CONFIG_FLAGS}
